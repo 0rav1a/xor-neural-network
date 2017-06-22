fef4e40bf5a611f1e14abda98fbccf41fd6e57d5
0rav1a <a.berruezo97@gmail.com>
2017-06-22 20:09:24 +0200

    Bias es una neurona más, en vez de un atributo

--- Network.py
+++ Network.py
@@ -3,35 +3,35 @@ import random
 from Neuron import Neuron
 from lib import gauss, sigmoid
 
-LEARNING_RATE = 3 #Para gradient descent
-ERROR = 0.05 #Margen de error de la red a partir del cual se considerará entrenada
+LEARNING_RATE = 1 #Para gradient descent
+ERROR = 0.001 #Margen de error de la red a partir del cual se considerará entrenada
 
 class Network:
-    def __init__(self, nNeurons = [1, 1, 1], layers = None):
+    def __init__(self, nNeurons):
         '''
         nNeurons: Número de neuronas que habrá en cada capa. La longitud de esta lista es el número de capas de la red.
         layers: Conjunto ya creado de capas (se usa al clonar una red)
         '''
-        self.layers = layers
         self.nNeurons = nNeurons
         self.error = 1
         
-        if (not layers):
-            self.layers = [[] for _ in nNeurons]
-            
-            for _ in range(nNeurons[0]): #Capa input
-                self.layers[0].append(Neuron([], self.layers[1]))
-    
-            for l in range(len(nNeurons))[1:-1]:
-                for _ in range(nNeurons[l]): #Capas hidden
-                    self.layers[l].append(Neuron(self.layers[l-1], self.layers[l+1]))
-                    
-            for _ in range(nNeurons[-1]): #Capa output
-                self.layers[-1].append(Neuron(self.layers[-2], []))
+        self.layers = [[] for _ in nNeurons]
+        self.bias = Neuron([], [])
+        self.bias.setOutput(1)
+        
+        for _ in range(nNeurons[0]): #Capa input
+            self.layers[0].append(Neuron([], self.layers[1]))
+
+        for l in range(len(nNeurons))[1:-1]:
+            for _ in range(nNeurons[l]): #Capas hidden
+                self.layers[l].append(Neuron(self.layers[l-1]+[self.bias], self.layers[l+1]))
+                
+        for _ in range(nNeurons[-1]): #Capa output
+            self.layers[-1].append(Neuron(self.layers[-2]+[self.bias], []))
     
     def setInputs(self, inputs):
         for i in range(self.nNeurons[0]):
-            self.layers[0][i].setBias(inputs[i])
+            self.layers[0][i].setOutput(inputs[i])
             
     def setTargets(self, targets):
         for o in range(self.nNeurons[-1]):
@@ -43,9 +43,6 @@ class Network:
                 neuron.calcOutput()
                 
         self.outputs = [neuron.getOutput() for neuron in self.layers[-1]]
-        
-    def getOutputs(self):
-        return self.outputs
             
     def backprop(self, tests, lr = LEARNING_RATE):
         '''
@@ -54,6 +51,7 @@ class Network:
         '''
         while self.error > ERROR:
             self.error = 0
+            random.shuffle(tests)
             for inputs, targets in tests: #Para cada iteración se realizan todos los tests
                 self.setInputs(inputs)
                 self.setTargets(targets)
@@ -61,16 +59,18 @@ class Network:
                 
                 for layer in self.layers[::-1]: #Las capas se recorren al revés para calcular los errores (backpropagation)
                     for neuron in layer:
-                        neuron.calcNeuronError()
-                        neuron.calcBiasError()
+                        neuron.calcError()
                         neuron.calcWeightsErrors()
                         
                 for layer in self.layers:
                     for neuron in layer:
-                        neuron.update(lr)
+                        neuron.updateWeights(lr)
                         
                 for o in range(self.nNeurons[-1]):
                     self.error += abs(self.outputs[o] - targets[o])
             
             self.error /= len(tests)*self.nNeurons[-1]
             print "\nError:\t" + str(self.error)
+            print "LR:\t" + str(lr)
+            
+    def getOutputs(self): return self.outputs
--- Neuron.py
+++ Neuron.py
@@ -4,9 +4,9 @@ from lib import sigmoid
 
 class Neuron:
     '''
-    Neurona con varias entradas y una salida
-    Cada entrada tiene asignada un peso, y uno extra para el bias
-    Si no hay entradas a la neurona, su salida será el bias
+    Neurona con varias entradas, un peso asignado a cada una, y una salida
+    Una de las entradas será bias (1 constante)
+    Si la neurona está en la capa input no tendrá entradas
     '''
     def __init__(self, prevLayer, nextLayer):
         '''
@@ -15,52 +15,41 @@ class Neuron:
         '''
         self.prevLayer = prevLayer
         self.nextLayer = nextLayer
-        self.bias = random.uniform(-1,1)
         
         self.weights = {} #Diccionario de pesos, accesibles por la neurona de la capa anterior a la que están conectados
         for neuron in prevLayer:
             self.weights[neuron] = random.uniform(-1,1)
             
     def calcOutput(self):
-        if not self.prevLayer: self.output = self.bias
-        else:
+        if self.prevLayer:
             self.output = 0
             for neuron in self.prevLayer:
                 self.output += neuron.getOutput() * self.weights[neuron]
             
-            self.output += self.bias
             self.output = sigmoid(self.output)
         
-    def calcNeuronError(self):
+    def calcError(self):
         '''Calcula la derivada del error total de la red respecto de la salida de esta neurona
         '''
         if not self.nextLayer: self.error = self.output - self.target
         else:
             self.error = 0
             for neuron in self.nextLayer:
-                self.error += neuron.getBiasError() * neuron.getWeights()[self]
-            
-    def calcBiasError(self):
-        '''Calcula la derivada del error total de la red respecto del bias
-        '''
-        self.biasError = self.error * self.output * (1-self.output)
+                self.error += neuron.getError() * neuron.getOutput() * (1-neuron.getOutput()) * neuron.getWeights()[self]
         
     def calcWeightsErrors(self):
         '''Calcula la derivada del error total de la red respecto de los pesos de la neurona
         '''
-        self.weightsErrors = {neuron: self.biasError * neuron.getOutput() for neuron in self.prevLayer}
+        self.weightsErrors = {neuron: self.error * self.output * (1-self.output) * neuron.getOutput() for neuron in self.prevLayer}
         
-    def update(self, lr):
-        '''Actualiza los pesos y bias de la neurona según los errores ya calculados
+    def updateWeights(self, lr):
+        '''Actualiza los pesos de la neurona según los errores ya calculados
         '''
-        self.bias -= self.biasError * lr
         for neuron in self.prevLayer:
             self.weights[neuron] -= self.weightsErrors[neuron] * lr
         
-    def setBias(self, b): self.bias = b
+    def setOutput(self, o): self.output = o
     def setTarget(self, t): self.target = t
     def getWeights(self): return dict(self.weights)
     def getOutput(self): return self.output
-    def getNeuronError(self): return self.error
-    def getBiasError(self): return self.biasError
-    def getWeightsErrors(self): return self.weightsErrors
+    def getError(self): return self.error
