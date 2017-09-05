1532b9b5da183f9b3700539cc3f47c50d015716b
0rav1a <a.berruezo97@gmail.com>
2017-09-05 13:38:35 +0200

    Intento de que la red reconozca números en imágenes de 28x28

--- NPNetwork.py
+++ NPNetwork.py
@@ -1,10 +1,10 @@
 #coding:utf-8
 import random
 import numpy as np
-from math import e
+import matplotlib.pyplot as plt
 
-LEARNING_RATE = 0.1 #Para gradient descent
-MIN_ERROR = 0.01 #Margen de error de la red a partir del cual se considerará entrenada
+LEARNING_RATE = 0.005 #Para gradient descent
+MIN_ERROR = 0.1 #Margen de error de la red a partir del cual se considerará entrenada
 
 class NPNetwork:
     '''
@@ -14,11 +14,9 @@ class NPNetwork:
         '''
         nNeurons: Número de neuronas que habrá en cada capa. La longitud de esta lista es el número total de capas de la red
         '''
-        self.weights       = [2*np.random.random((nNeurons[l]+1, nNeurons[l+1]))-1 for l in range(len(nNeurons))[:-1]] #Pesos de salida de una neurona [capa][neurona][peso]
-        self.weightsErrors = [np.zeros((nNeurons[l]+1, nNeurons[l+1])) for l in range(len(nNeurons))[:-1]] #Derivada del error total de la red respecto de un peso [capa][neurona][peso]
-        self.outputs       = [np.zeros(n) for n in nNeurons] #Salidas calculadas de cada neurona [capa][neurona]
-        self.outputsErrors = [np.zeros(n) for n in nNeurons] #Derivada del error total de la red respecto de una salida [capa][neurona]
-        
+        self.weights = [2*np.random.random((nNeurons[l]+1, nNeurons[l+1]))-1 for l in range(len(nNeurons)-1)] #Pesos de salida de una neurona [capa][neurona][peso]. Incluye los pesos de salida de la neurona bias
+        self.outputs = [np.zeros(n) for n in nNeurons] #Salidas calculadas de cada neurona [capa][neurona]
+
     def calcOutputs(self):
         '''Calcula la salida de cada neurona de la red
         '''
@@ -30,33 +28,42 @@ class NPNetwork:
         Dada una lista de tests, se modifican los pesos y bias de todas las neuronas para que la salida se asemeje a la esperada
         tests: Listas de pares [entrada, salida esperada] para entrenar la red
         '''
-        error = 1.0
+        weightsErrors = [np.zeros((nNeurons[l]+1, nNeurons[l+1])) for l in range(len(nNeurons)-1)] #Derivada del error total de la red respecto de un peso [capa][neurona][peso]
+        outputsErrors = [np.zeros(n) for n in nNeurons] #Derivada del error total de la red respecto de una salida [capa][neurona]
+        #weightsIncr   = [np.zeros((nNeurons[l]+1, nNeurons[l+1])) for l in range(len(nNeurons)-1)] #Incremento de cada peso de la red (una vez calculado el error respecto de varios tests)
         cont = 0
-        while error > MIN_ERROR:
-            error = 0.0
+        currError = 1.0 #Error actual de la red (después de realizar todos los tests)
+        prevErrors = []
+
+        while currError > MIN_ERROR:
+            currError = 0.0
             for inputs, targets in tests: #En cada iteración se realizan todos los tests
                 self.setInputs(inputs)
                 self.calcOutputs() #Se necesita la salida de cada neurona
 
                 #Se calculan los errores de neuronas y pesos
-                self.outputsErrors[-1] = self.outputs[-1] - targets
+                outputsErrors[-1] = self.outputs[-1] - targets
                 for l in range(len(self.weights))[::-1]: #Se empieza por la última capa hasta la primera (backpropagation)
-                    self.weightsErrors[l] = np.outer(np.append(self.outputs[l], 1), self.outputsErrors[l+1] * self.outputs[l+1] * (1-self.outputs[l+1]))
-                    self.outputsErrors[l] = np.dot(self.weights[l][:-1], self.outputsErrors[l+1] * self.outputs[l+1] * (1-self.outputs[l+1]))
+                    weightsErrors[l] = np.outer(np.append(self.outputs[l], 1), outputsErrors[l+1] * self.outputs[l+1] * (1-self.outputs[l+1]))
+                    outputsErrors[l] = np.dot(self.weights[l][:-1], outputsErrors[l+1] * self.outputs[l+1] * (1-self.outputs[l+1]))
 
                 #Se actualizan los pesos según los errores calculados
                 for l in range(len(self.weights)):
-                    self.weights[l] -= self.weightsErrors[l] * LEARNING_RATE
+                    self.weights[l] -= weightsErrors[l] * lr
 
-                #Se calcula la salida de la red con los nuevos pesos para saber el error
+                #Se calcula la salida de la red con los nuevos pesos y se modifica el error
                 self.calcOutputs()
-                if max(abs(self.outputs[-1] - targets)) > error: error = max(abs(self.outputs[-1] - targets))
+                if np.argmax(self.outputs[-1]) != np.argmax(targets): error += 1
 
+            currError /= len(tests)
+            prevErrors.append(currError)
             cont += 1
-            print "\nError:\t" + str(error)
+            plt.plot(errors)
+            plt.pause(0.0000000001)
+            print "\nError:\t" + str(currError)
             print "LR:\t" + str(lr)
             print "Cont:\t" + str(cont)
 
     def setInputs(self, inputs): self.outputs[0] = inputs
     def getOutputs(self): return self.outputs[-1]
-def sigmoid(x): return 1/(1+e**-x)
+def sigmoid(x): return 1/(1+np.exp(-x))
---
+++ imagenes.py
@@ -0,0 +1,81 @@
+#coding:utf-8
+from PIL import Image
+from NPNetwork import NPNetwork
+import numpy as np
+
+imagenes = open("train-images.idx3-ubyte", "rb")
+imagenes.read(16)
+labels = open("train-labels.idx1-ubyte", "rb")
+labels.read(8)
+test = []
+
+for _ in range(1000):
+    img = []
+    for _ in range(784):
+        img.append(ord(imagenes.read(1)))
+    lbl = [0,0,0,0,0,0,0,0,0,0]
+    lbl[ord(labels.read(1))] = 1
+
+    test.append([img,lbl])
+
+test = np.array(test)
+
+net = NPNetwork([784,100,10])
+net.backprop(test)
+
+for inputs, targets in test:
+    net.setInputs(inputs)
+    net.calcOutputs()
+    if np.argmax(net.getOutputs()) != np.argmax(targets):
+        raw_input()
+        img = Image.new("P", (28,28))
+        for c in range(28):
+            for f in range(28):
+                img.putpixel((f,c), inputs[28*c+f])
+        print net.getOutputs()
+        img.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+raw_input("FIN")
+
+for _ in range(20):
+    img = Image.new("P", (28,28))
+    for c in range(28):
+        for f in range(28):
+            img.putpixel((f,c), ord(imagenes.read(1)))
+    img.show()
+    raw_input(ord(labels.read(1)))
+
+
+image = Image.open("4.png")
+img = image.load()
+
+imgList = []
+for f in range(image.size[0]):
+    for c in range(image.size[1]):
+        if img[f,c][0] == 0: imgList.append(1)
+        elif img[f,c][0] == 255: imgList.append(0)
+
+net = NPNetwork([8100,8100,1])
+net.backprop([(imgList,[1])])
